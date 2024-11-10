061ac734be3b66a87dbccdcc921bdcea6c5e6ead
Adam DJ Brett <adamdjbrett@users.noreply.github.com>
2024-11-09 21:51:37 -0600

    :rocket: initial commit
    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# 11covers-theme
+# 11ty Cover Theme
+A hopefully stylish 11ty **minor** update to the common cover card theme type. This 11ty theme is based on:
+1. [Front Cover Jekyll Theme](https://github.com/dashingcode/front-cover) by [@dashingcode](https://github.com/dashingcode/front-cover)
+2. [@epidrome's cover-card Fork of Front Cover Jekyll Theme](https://github.com/epidrome/cover-card) by [@epidrome](https://github.com/epidrome/cover-card)
+3. [Eleventy Base Blog Starter Project](https://github.com/11ty/eleventy-base-blog)
+
+## Credits
+1. Adam DJ Brett
+2. [Creative by Dre](https://www.creativitas.dev/)
+
+## Image Credits
+- Profile Avatar by This Person Does Not Exist
+- Background photo NASA/JPL-Caltech, October 30, 2013
+
+## Setup
+
+### eleventy.config.js
+in `eleventy.config.js` go to metadata and personalize lines 64-73. Keep it short and simple
+
+### Metadata.js
+the majority of the personalization changes you will need to make will be to `metadata.js`
+
+### Font Awesome
+Once you have finished your modifications to `metadata.js` visit [Font Awesome](https://fontawesome.com/) and 
+1. create a free account
+2. create a free or paid [Font Kit](https://fontawesome.com/kits)
+3. Once you have created your font kit paste the code to `base.njk` layout line 38
+
+### humans.njk
+Now update humans.njk and give credit to all of the folks who have helepd to make your site an dlet everyone know that a real human built your site and not AI.
+
+### Publication
+Once you have completed all of these steps now you are ready to publish your site to Cloudflare Pages, Github Pages, Netlify, Vercel, Surge, Render, or your prefered static site hosting provider.
+
---
+++ _data/metadata.js
@@ -0,0 +1,71 @@
+export default {
+	title: "Kathryn Janeway",
+	url: "https://cover.000000076.xyz",
+	language: "en",
+	locale: "en-US",
+	description: "Captain of the Intrepid-class USS Voyager in 2371",
+    avatar: "/img/avatar.jpg",
+    cover: "/img/witch-head-brews-baby-stars-1920x1152.jpg",
+    github_username:  "11ty",
+    favicon: "/img/favicon.png",
+    twitter_username: "eleven_ty",
+    facebook_username: "eleven_ty",
+    facebook_appid: "",
+    footer:  {
+		copyrights: "MIT License/CC BY 4.0 License",
+		text_link: "Kathryn Janeway",
+		url: "https://en.wikipedia.org/wiki/Kathryn_Janeway"
+	},
+	author: {
+		name: "Kathryn Janeway",
+		email: "info@example.com",
+		url: "Kathryn Janeway"
+	},
+	social: [
+              {
+                icon: "globe",
+                description: "Main website",
+                link: "https://www.11ty.dev"
+              },
+              {
+                icon: "github",
+                description: "Follow 11ty on Github",
+                link: "https://github.com/11ty/eleventy/"
+              },
+              {
+                icon: "mastodon",
+                description: "11ty on Mastodon",
+                link: "https://fosstodon.org/@eleventy"
+              },
+              {
+                icon: "bluesky",
+                description: "11ty on Bluesky",
+                link: "https://bsky.app/profile/11ty.dev"
+              },
+              {
+                icon: "youtube",
+                description: "11ty on YouTube",
+                link: "https://www.youtube.com/c/EleventyVideo"
+              },              {
+                icon: "discord",
+                description: "11ty on Discord",
+                link: "https://www.11ty.dev/blog/discord/"
+              },
+              {
+                icon: "twitter",
+                description: "11ty on Twitter",
+                link: "https://twitter.com/leven_ty"
+              },
+              {
+                icon: "codepen",
+                description: "11ty Codepen",
+                link: "https://codepen.io/search/pens?q=11ty"
+              },
+              {
+                icon: "tags",
+                description: "11ty Blogs",
+                link: "https://www.11ty.dev/blog/"
+              }
+    ]
+
+}
